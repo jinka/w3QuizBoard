fe7939bcd90dace6f3607c001261dff2cef749c2
dayud mohamed <voiphandy@gmail.com>
2018-12-11 10:08:05 +0300

    Created variable to hold old qustions as an object and added questions in  quiz.html

--- js/scripts.js
+++ js/scripts.js
@@ -3,5 +3,54 @@ $(document).ready(function() {
   var quizBoard = document.getElementById('quiz');
   var resultBoard = document.getElementById('results');
   var finishButton = document.getElementById('submit');
+  var allQuestions = [{
+      question: "What do dragonflies prefer to eat?",
+      answers: {
+        a: "Bee",
+        b: "Dogs",
+        c: "Mosquitoes"
+      },
+      correctAnswer: "c"
+    },
+    {
+      question: "Which is the largest species of the tiger?",
+      answers: {
+        a: "Asian Tiger",
+        b: "African tiger",
+        c: "Siberian tiger"
+      },
+      correctAnswer: "c"
+    },
+    {
+      question: "In what year was Google launched on the web?",
+      answers: {
+        a: "1980",
+        b: "1990",
+        c: "2001",
+        d: "1998"
+      },
+      correctAnswer: "d"
+    },
+    {
+      question: "What is the country top-level domain of Kenya?",
+      answers: {
+        a: ".co",
+        b: ".ke",
+        c: ".co.uk",
+        d: ".co.ke"
+      },
+      correctAnswer: "b"
+    },
+    {
+      question: "In computing what is Ram short for?",
+      answers: {
+        a: "Random Access Memory",
+        b: "Read Only Memory",
+        c: "Read Access Momory",
+        d: "None"
+      },
+      correctAnswer: "a"
+    }
+  ];
 
 });
